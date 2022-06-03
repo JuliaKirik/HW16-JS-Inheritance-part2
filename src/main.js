e3a358bd1da02c44b2f3e4b8310138f07c279ad5
JuliaKirik <juliakirichok9878@gmail.com>
2022-06-03 19:18:36 +0300

    Added

---
+++ src/main.js
@@ -0,0 +1,45 @@
+class Clock {
+    constructor(clockNode) {
+        this.clockNode = clockNode;
+        this.formatFull = true;
+        clockNode.addEventListener('click', () => {
+        this.formatFull = !this.formatFull;
+        });
+    }
+    render() {
+        const date = new Date(),
+            hours = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours(),
+            minutes = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes(),
+            seconds = (date.getSeconds() < 10) ? '0' + date.getSeconds() : date.getSeconds();
+        if (!this.formatFull) {
+            this.clockNode.innerHTML = hours + ':' + minutes;
+        } else {
+            this.clockNode.innerHTML = hours + ':' + minutes + ':' + seconds;
+        }
+    }
+    start() {
+        this.render();
+        this.timer = setInterval(() => this.render(), 1000);
+    }
+}
+
+class FullFormatClock extends Clock{
+    constructor(clockNode) {
+        super (clockNode);
+    }
+}
+class ShortFormatClock extends Clock{
+    constructor(clockNode) {
+        super (clockNode);
+        this.formatFull = false;
+    }
+}
+
+const clock1 = new Clock(document.querySelector('#clock1'));
+clock1.start();
+
+const fullFormatClock = new FullFormatClock(document.querySelector('#clock2'));
+fullFormatClock.start();
+
+const shortFormatClock = new ShortFormatClock(document.querySelector('#clock3'));
+shortFormatClock.start();
